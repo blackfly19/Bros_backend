13ee547fff2bdca367d3f82413d32b595e4e25fd
blackfly19 <43345480+blackfly19@users.noreply.github.com>
2020-09-09 16:11:14 +0530

    Updated database
    Added the postgresql database to the app

--- BasicChat/__init__.py
+++ BasicChat/__init__.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "47087a547430f78b85c5436470c0d88d"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 socketio = SocketIO(app,cors_allowed_origins='*')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///chat_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 from BasicChat import routes
