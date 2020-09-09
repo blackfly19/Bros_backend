ab631cc3f938f7e5a34b7fa4b0a28f08453d51ff
Ankit Chawla <ankitc226@gmail.com>
2020-09-09 18:07:48 +0530

    Added commands.py

--- BasicChat/commands.py
+++ BasicChat/commands.py
@@ -2,7 +2,7 @@ import click
 from flask.cli import with_appcontext
 
 from BasicChat import db
-from BasicChat.models import User
+from BasicChat.models import History
 
 @click.command(name='create_tables')
 @with_appcontext
