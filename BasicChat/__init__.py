52c6dbfdf3f0f7ed65538d1173396acf177c3f11
Ankit Chawla <ankitc226@gmail.com>
2020-09-09 18:20:33 +0530

    Minor changes

--- BasicChat/__init__.py
+++ BasicChat/__init__.py
@@ -9,4 +9,5 @@ socketio = SocketIO(app,cors_allowed_origins='*')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
+from BasicChat import commands
 from BasicChat import routes
