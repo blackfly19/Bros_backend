36fb7e9e1ffbeba2b5f564af8fef42ee6b5e736b
Ankit Chawla <ankitc226@gmail.com>
2020-09-09 18:25:46 +0530

    Minor changes

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from BasicChat import app,socketio
+from BasicChat import app,socketio,db
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     socketio.run(app,debug=True)
