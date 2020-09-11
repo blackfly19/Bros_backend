2063227cee30f3e4890c794c3d865261c65553cf
Ankit Chawla <ankitc226@gmail.com>
2020-09-11 10:52:41 +0530

    Resolved casing issues

--- BasicChat/routes.py
+++ BasicChat/routes.py
@@ -6,20 +6,6 @@ import json
 import random
 
 global_chat_users = {}
-colors = {
-    'Ali': 'blue',
-    'Ankit':'red',
-    'Sarvesh':'green',
-    'Abhishek':'yellow',
-    'Abhigyan': 'orange'
-}
-
-"""def generate_color():
-
-    colors = ['green','blue','pink','black','cyan','purple','red','orange','brown','yellow']
-    rnd = random.randint(0,9)
-    return colors[rnd]"""
-
 
 @socketio.on('message')
 def handleMessage(data):
@@ -30,8 +16,7 @@ def handleMessage(data):
         db.session.add(message)
         db.session.commit()
         last_message_id = History.query.order_by(History.id.desc()).first().id
-        color = colors[global_chat_users[request.sid]]
-        js = {"id": last_message_id,"message":data[1],"color":color,"username":global_chat_users[request.sid],"time":data[2]}
+        js = {"id": last_message_id,"message":data[1],"color":'black',"username":global_chat_users[request.sid],"time":data[2]}
         send_json = json.dumps(js)
         #print(send_json)
         send(send_json,broadcast=True,include_self=False)
@@ -48,7 +33,7 @@ def getDeviceId(values):
     messages = History.query.all()
     if messages is not None:
         for m in messages:
-            js = {"id":m.id,"message":m.message,"color":colors[m.username],"username":m.username,"time":m.time}
+            js = {"id":m.id,"message":m.message,"color":'black',"username":m.username,"time":m.time}
             send_json = json.dumps(js)
             send(send_json,include_self=True)
 
